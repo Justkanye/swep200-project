b0294060d67457c5e8cc409864755d437b8607be
Olawuyi Ismail <Olaism01@gmail.com>
2021-12-08 07:39:00 +0100

    Seettings updated

--- student_management_system/wsgi.py
+++ student_management_system/wsgi.py
@@ -8,13 +8,13 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-from django.core import settings
+# from django.core import settings
 from django.core.wsgi import get_wsgi_application
 
-from whitenoise import WhiteNoise
+?from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'student_management_system.settings')
 
 application = get_wsgi_application()
 
-application = WhiteNoise(application, root=settings.STATIC_ROOT)
+# application = WhiteNoise(application, root=settings.STATIC_ROOT)
