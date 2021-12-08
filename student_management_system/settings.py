8d7ef69a865a242721b195e7e06d7044298fe030
Olawuyi Ismail <Olaism01@gmail.com>
2021-12-08 03:23:20 +0100

    Add Allowed host

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = '(i#*06f#keydy_fh17bf=$0f6v)^wr^l7*u4gq42m*sztu#2_m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["swep200group5.herokuapp.com"]
 
 
 # Application definition
