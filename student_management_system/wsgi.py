4adab13fb024a016f9208438bf7f178d378f2232
Olawuyi Ismail <Olaism01@gmail.com>
2021-12-09 17:44:27 +0100

    Debug

--- student_management_system/wsgi.py
+++ student_management_system/wsgi.py
@@ -8,14 +8,10 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-# from django.core import settings
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
 # from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'student_management_system.settings')
 
 application = get_wsgi_application()
-
-application = DjangoWhiteNoise(application)
